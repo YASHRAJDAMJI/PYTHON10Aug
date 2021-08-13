ae441c3132a4a2605f5c8416116b114bf368d843
YASHRAJDAMJI <damjisb@gmail.com>
2021-08-13 12:54:10 +0530

    Quations Commit

---
+++ operators.py
@@ -0,0 +1,52 @@
+
+#Arithmatic operators
+x,y,z=20,50,30
+
+print("20+50 = ",x+y)
+
+print("20-50 = ",z-y)
+
+print("20*50 = ",x*y)
+
+print("30/20 = ",z/x)
+
+
+#comparison opretor
+
+print(" x>y",x>y)
+print(" x<y",x<y)
+print(" x==y",x>y)
+
+#logical operator
+
+a=True
+b=False
+
+print("a and b is ",a and b)
+print("a or b is ",a or b)
+print("not b is ",not b)
+
+#Identity operators
+
+ x1=5
+ x2="Hello"
+ x3=[1,2,3]
+
+ y1=5
+ y2="Hello"
+ y3=[1,2,3]
+
+ print("x1 is not y2",x1 is not y2)
+ print("x1 is not y1",x1 is not y1)
+ print("x2 is y2",x2 is y2)
+
+
+ #membership Operators
+
+ m1="Hello World"
+ m2=[1:'a',2:'b']
+
+ print('H' in m1)
+ print('Hello' not in m1)
+ print(1 in m2)
+ print('a'in m2)
