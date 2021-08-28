b2ac7bba3759fa2b5c78e5c6885e9b190bb55032
YASHRAJDAMJI <damjisb@gmail.com>
2021-08-28 17:44:25 +0530

    Assignment 2

---
+++ Assingment 2/areapentagon/areapen.py
@@ -0,0 +1,8 @@
+
+def arpen():
+    s= int(input("Enter side of the  Pentagon: "))
+    a= int(input("Enter apothem length of Pentagon: "))
+
+    areaofpen=(5.0/2.0)*s*a
+
+    print("Area of Pentagon is  ",areaofpen)
